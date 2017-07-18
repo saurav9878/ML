79be3e575e652979b24fd6e79255d4bd1d33f4b3
Saurav <saurav9878@gmail.com>
2017-07-18 11:22:19 +0530

    Vectorized

--- ex1/gradientDescent.m
+++ ex1/gradientDescent.m
@@ -15,7 +15,7 @@ for iter = 1:num_iters,
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-     theta = theta -(((X * theta) - y)' * X)' * alpha/m;
+     theta = theta -(((X * theta) - y)' *X)' * alpha/m;
     % ============================================================
 
     % Save the cost J in every iteration    
